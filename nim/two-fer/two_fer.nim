8dc4ad0c3adb230188f3efa8b276edaae81e019e
Aron Neagu <aron.neagu@gmail.com>
2020-03-21 21:39:25 +0000

    simpler solution for two fer, add default for proc parameter, and no need to use result =, simple expressions work just as fine

--- nim/two-fer/two_fer.nim
+++ nim/two-fer/two_fer.nim
@@ -1,5 +1,2 @@
-proc twoFer*(name: string): string =
-  result = "One for " & name & ", one for me."
-
-proc twoFer*(): string =
-  result = "One for you, one for me."
+proc twoFer*(name: string = "you"): string =
+  "One for " & name & ", one for me."
