573e9a42f7452c8e45dd23e77db55ca5ed9fb577
Aron Neagu <aron.neagu@gmail.com>
2020-03-23 20:32:08 +0000

    working isogram

---
+++ nim/isogram/isogram.nim
@@ -0,0 +1,6 @@
+import sets, strutils
+proc isIsogram*(str: string): bool =
+  let 
+    s1 = str.multiReplace(("-",""),(" ",""))
+    s2 = str.toLowerAscii.multiReplace(("-",""),(" ","")).toHashSet
+  if s1.len == s2.len: return true
