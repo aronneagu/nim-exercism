d30b9d3142a806b137e7164c7cceac2780cb8fbb
Aron Neagu <aron.neagu@gmail.com>
2020-03-24 11:27:41 +0000

    improved difference of squares

--- nim/difference-of-squares/difference_of_squares.nim
+++ nim/difference-of-squares/difference_of_squares.nim
@@ -1,11 +1,11 @@
-proc sumOfSquares*(x: int): int =
+func sumOfSquares*(x: int): int =
   for i in 1..x:
     result += i * i
 
-proc squareOfSum*(x: int): int =
+func squareOfSum*(x: int): int =
   for i in 1..x:
     result += i
-  return result * result
+  result * result
 
-proc difference*(x: int): int =
-  return squareOfSum(x) - sumOfSquares(x)
+func difference*(x: int): int =
+  squareOfSum(x) - sumOfSquares(x)
